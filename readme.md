ab0a7495dd837ce0725d05230e9acbb897b479df
Robert Hull <roberthull@email.arizona.edu>
2023-01-20 09:36:59 -0700

    updates to scripts and files

---
+++ readme.md
@@ -0,0 +1,4 @@
+# re_app
+For rivereyes
+
+go to https://github.com/rhull21/re_app/tree/main/site_v1/reyes
--- scripts/readheatmap.py
+++ scripts/readheatmap.py
@@ -22,23 +22,33 @@ print(arr_all.shape)
 print(plot_dict.keys())
 
 # %%
-wb = Workbook()
-ws = wb.active
+# wb = Workbook()
+# ws = wb.active
 
-with pd.ExcelWriter(os.path.join(dir,nm+'.xlsx'), engine="openpyxl") as writer:
-    writer.book = wb
-    writer.sheets = dict((ws.title, ws) for ws in wb.worksheets)
-    for i in range(arr_all.shape[2]):
-        df = pd.DataFrame(arr_all[:,:,i],index=plot_dict['River Miles'],columns=plot_dict['strf_dates'])
-        df.to_excel(writer, sheet_name=str(plot_dict['Years'][i]))
+# with pd.ExcelWriter(os.path.join(dir,nm+'.xlsx'), engine="openpyxl") as writer:
+#     writer.book = wb
+#     writer.sheets = dict((ws.title, ws) for ws in wb.worksheets)
+#     for i in range(arr_all.shape[2]):
+#         df = pd.DataFrame(arr_all[:,:,i],index=plot_dict['River Miles'],columns=plot_dict['strf_dates'])
+#         df.to_excel(writer, sheet_name=str(plot_dict['Years'][i]))
         
 
 # %%
 
-arr_all = arr_all.reshape((arr_all.shape[0],arr_all.shape[1]*arr_all.shape[2]))
-columns =   [
-                strf_date+'-'+str(yr) for strf_date in plot_dict['strf_dates'] for yr in plot_dict['Years'] 
+# arr_all2 = arr_all.reshape((arr_all.shape[0],arr_all.shape[1]*arr_all.shape[2]))
+# columns =   [
+#                 strf_date+'-'+str(yr) for strf_date in plot_dict['strf_dates'] for yr in plot_dict['Years'] 
+#             ]
+
+# %%
+arr_all2 = np.zeros((arr_all.shape[0],arr_all.shape[1]*arr_all.shape[2]))
+for i in range(arr_all.shape[2]):
+    arr_all2[:,i*len(plot_dict['strf_dates']):(i+1)*len(plot_dict['strf_dates'])] = arr_all[:,:,i]
+    
+columns2 =   [
+                strf_date+'-'+str(yr) for yr in plot_dict['Years'] for strf_date in plot_dict['strf_dates']  
             ]
+
 
 
 # %%
@@ -48,7 +58,7 @@ ws = wb.active
 with pd.ExcelWriter(os.path.join(dir,nm+'_flat.xlsx'), engine="openpyxl") as writer:
     writer.book = wb
     writer.sheets = dict((ws.title, ws) for ws in wb.worksheets)
-    df = pd.DataFrame(arr_all,index=plot_dict['River Miles'],columns=columns)
+    df = pd.DataFrame(arr_all2,index=plot_dict['River Miles'],columns=columns2)
     df.to_excel(writer)
         
 # %%
