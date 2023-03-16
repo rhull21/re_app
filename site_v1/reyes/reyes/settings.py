059cc50ad8219b3616f51bac08d9c81cde009153
root <roberthull@email.arizona.edu>
2023-03-16 17:11:22 +0000

    updates to publishing branch

--- site_v1/reyes/reyes/settings.py
+++ site_v1/reyes/reyes/settings.py
@@ -16,7 +16,8 @@ import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-etc_dir = '../../etc'
+#etc_dir = '../../etc'
+etc_dir = '/var/www/re_app_pub/etc'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -151,6 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'riogrande/static/') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
