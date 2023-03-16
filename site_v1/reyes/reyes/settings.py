52dec24c84d256668b5aee28f0b59e13802f2885
Robert Hull <roberthull@email.arizona.edu>
2023-03-16 10:44:41 -0700

    this is an important change to make sure settings work universially

--- site_v1/reyes/reyes/settings.py
+++ site_v1/reyes/reyes/settings.py
@@ -16,8 +16,8 @@ import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-#etc_dir = '../../etc'
-etc_dir = '/var/www/re_app_pub/etc'
+etc_dir = '../../etc'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
